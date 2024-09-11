d47e349bbbe2b18916c4967baca1656937086710
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2024-09-11 06:16:45 +0500

    footer

--- Frontend/my-app/src/Footer/Footer.js
+++ Frontend/my-app/src/Footer/Footer.js
@@ -1,7 +1,6 @@
-
-import { FaUser, FaArrowRight } from 'react-icons/fa';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPen } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faUserPen, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FaArrowRight } from 'react-icons/fa';
 
 export default function Footer(){
 
@@ -10,24 +9,81 @@ export default function Footer(){
         <>
         
         
-        <div className="flex items-center space-x-12 justify-center px-4 py-2 bg-[#017aaf]">
+  
+
+
+
+
+{/* <div className="bg-[#017aaf] py-2 px-4">
+  <div className="flex flex-col items-center justify-between max-w-screen-xl mx-auto space-y-4 text-white lg:flex-row lg:space-y-0">
+  
+    <div className="flex items-center space-x-4">
+      <FontAwesomeIcon icon={faEnvelope} className="w-6 h-6" />
+      <span className="text-base lg:text-xl">abc@gmail.com</span>
+    </div>
+
+    <div className="flex items-center space-x-12">
      
-      <div className="flex items-center mr-6 space-x-2 text-white ">
-        <span className='text-xl'>Registration</span>
-        <FontAwesomeIcon icon={faUserPen} className="w-8 h-8 text-white transition-all duration-200 cursor-pointer hover:scale-110" />
-
-
+      <div className="flex items-center space-x-2">
+        <span className="text-base lg:text-xl">Registration</span>
+        <FontAwesomeIcon icon={faUserPen} className="w-8 h-8 transition-transform duration-200 cursor-pointer hover:scale-110" />
       </div>
 
-     
-      <div className="flex items-center space-x-2 text-white">
-        <span className='text-xl'>Login</span>
+      <div className="flex items-center space-x-2">
+        <span className="text-base lg:text-xl">Login</span>
         <div className="flex items-center justify-center w-10 h-10 border-2 border-white rounded-full">
-          <FaArrowRight className="w-8 h-8 text-white transition-all duration-200 cursor-pointer hover:scale-110" />
+          <FaArrowRight className="w-8 h-8 transition-transform duration-200 cursor-pointer hover:scale-110" />
         </div>
       </div>
     </div>
+
+
+    <div className="flex items-center space-x-4 ">
+      <FontAwesomeIcon icon={faPhone} className="w-6 h-6" />
+      <span className="text-base lg:text-xl">000-111-222</span>
+    </div>
+  </div>
+</div> */}
         
+        
+        <div className="bg-[#017aaf] py-2 px-4">
+      <div className="flex flex-col items-center max-w-screen-xl mx-auto space-y-4 text-white">
+        
+   
+        <div className="flex space-x-8">
+ 
+          <div className="flex items-center space-x-2">
+            <span className="text-base lg:text-xl">Registration</span>
+            <FontAwesomeIcon icon={faUserPen} className="w-8 h-8 transition-transform duration-200 cursor-pointer hover:scale-110" />
+          </div>
+
+      
+          <div className="flex items-center space-x-2">
+            <span className="text-base lg:text-xl">Login</span>
+            <div className="flex items-center justify-center w-10 h-10 border-2 border-white rounded-full">
+              <FaArrowRight className="w-8 h-8 transition-transform duration-200 cursor-pointer hover:scale-110" />
+            </div>
+          </div>
+        </div>
+
+
+        <div className="flex space-x-8">
+       
+          <div className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faEnvelope} className="w-6 h-6" />
+            <span className="text-base lg:text-xl">abc@gmail.com</span>
+          </div>
+
+    
+          <div className="flex items-center space-x-2">
+            <FontAwesomeIcon icon={faPhone} className="w-6 h-6" />
+            <span className="text-base lg:text-xl">000-111-222</span>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+
         
         </>
     )
